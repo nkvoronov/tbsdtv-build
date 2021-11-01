c5596dea1a2d5057ff2afbb4524a2ff45aaaaa5e
Nikolay Voronov <nkvoronov@gmail.com>
2021-11-01 23:36:43 +0200

    init

---
+++ firmware.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ROOT="/media/Devel/tbsdtv-build"
+DIR_FIRMWARE="firmware"
+
+#  get firmware
+if [ -d $DIR_FIRMWARE ]; then
+  rm -rf $DIR_FIRMWARE
+fi
+mkdir $DIR_FIRMWARE
+
+wget http://www.tbsdtv.com/download/document/linux/tbs-tuner-firmwares_v1.0.tar.bz2
+tar jxvf tbs-tuner-firmwares_v1.0.tar.bz2 -C $ROOT/$DIR_FIRMWARE
